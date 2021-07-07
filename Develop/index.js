00eb8da4badbf963d15a1f7bbd6cf05227990298
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-07 01:02:24 -0500

    added the packages to run and first few questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,28 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateReadme = require('./utils/generateMarkdown');
+const createFile = require('./utils/file-generator.Readme.md')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please input your Gihub username.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please input your email.'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please provide the name of your project'
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
